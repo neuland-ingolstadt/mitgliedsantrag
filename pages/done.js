c24ed49394d27d3f7c795abca1257d61a1d63281
Alexander Horn <alexhorn@users.noreply.github.com>
2021-04-25 17:51:08 +0200

    Remove voice chat link on /done

--- pages/done.js
+++ pages/done.js
@@ -9,14 +9,6 @@ export default function Done () {
           Dein Mitgliedsantrag ist bei uns eingegangen!<br />
           Wir werden uns so schnell wie möglich bei dir melden. :)
         </p>
-        <p>
-          In der Zwischenzeit kannst du uns in unserem Voice-Chat besuchen:
-        </p>
-        <p>
-          <a href="https://voice.informatik.sexy" target="_blank" rel="noreferrer">
-            <button className="btn btn-primary btn-ghost">voice.informatik.sexy</button>
-          </a>
-        </p>
       </div>
     </Container>
   )
