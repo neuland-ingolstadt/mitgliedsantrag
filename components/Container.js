bdad78d3b8f3ce48d9712cb5ae9920d1d4d81215
Alexander Horn <alexhorn@users.noreply.github.com>
2022-10-06 17:26:14 +0200

    Replace favicon with SL logo

--- components/Container.js
+++ components/Container.js
@@ -11,7 +11,7 @@ export default function Container ({ title, children }) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <div className="container">
         <h2 className='heading'>
